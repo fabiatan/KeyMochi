2c7fe76ad1f790e1ac474323a36a38771a7458a3
Fabian <fnever520@gmail.com>
2026-04-20 09:23:18 +0800

    Initial Commit

---
+++ MechSpatial/MechSpatialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MechSpatialApp.swift
+//  MechSpatial
+//
+//  Created by Fabian Tan on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MechSpatialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MechSpatialTests/MechSpatialTests.swift
@@ -0,0 +1,19 @@
+//
+//  MechSpatialTests.swift
+//  MechSpatialTests
+//
+//  Created by Fabian Tan on 20/04/2026.
+//
+
+import Testing
+@testable import MechSpatial
+
+struct MechSpatialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
